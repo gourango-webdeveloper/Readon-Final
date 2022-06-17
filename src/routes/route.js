edee6605ec61e2926ad611b3a9ed6499b82722cb
gourango-webdeveloper <gourangodebnath80@gmail.com>
2022-06-17 15:51:41 +0530

    done covaction data fatch

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,10 +13,11 @@ router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
 
+router.get("/cowin/getByDistrict",CowinController.getByDistrictId)
+
 router.post("/cowin/getOtp", CowinController.getOtp)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-
 
 
 module.exports = router;
